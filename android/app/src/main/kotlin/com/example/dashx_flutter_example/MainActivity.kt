810672522fd5227bce5f9826c47a498bcfcaab63
Aatika Khan <aatikakhan869@gmail.com>
2022-05-23 13:33:47 +0530

    demo app to test

---
+++ android/app/src/main/kotlin/com/example/dashx_flutter_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.dashx.demo_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constant.dart
@@ -0,0 +1,5 @@
+// TODO: set your constant keys here
+
+String publicKey = "";
+String baseUri = "";
+String targetEnvironment = "";
