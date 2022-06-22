47386cbcb09d12a52c7cf199594bb8b0d10f4233
Aatika Khan <aatikakhan869@gmail.com>
2022-06-23 00:23:35 +0530

    Update in design with respect to functionalities & flow

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,8 @@ import 'dart:async';
 
 import 'package:flutter/services.dart';
 
+import 'home_screen.dart';
+
 void main() async {
   runApp(const MyApp());
 }
@@ -17,8 +19,28 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: SampleApp());
+  }
+}
+
+class SampleApp extends StatefulWidget {
+  const SampleApp({Key? key}) : super(key: key);
+
+  @override
+  State<SampleApp> createState() => _SampleAppState();
+}
+
+class _SampleAppState extends State<SampleApp> {
   String message = 'Unknown';
   String uid = '';
+  String password = '';
+  String email = '';
+  String error_message = '';
+  TextEditingController email_controller = TextEditingController();
+  TextEditingController password_controller = TextEditingController();
+
   DashXPlugin dashxFlutterPlugin = DashXPlugin();
   DashX dx = DashX(
     publicKey: publicKey,
@@ -54,47 +76,95 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        title: const Text('Plugin example app'),
-      ),
+    return Scaffold(
+      appBar: AppBar(title: Text('Plugin Eample App')),
       body: Center(
-          child: Column(
-        children: [
-          const Padding(
-            padding: EdgeInsets.all(16.0),
-            child: Text('Anonymous UID'),
-          ),
-          Text(uid.toString()),
-          TextButton(
-              onPressed: () {
-                dx.identify();
-              },
-              child: const Text("Identify ")),
-          TextButton(
-              onPressed: () {
-                dx.setIdentity("", "");
-              },
-              child: const Text("Set Identity")),
-          TextButton(
-            onPressed: () {},
-            child: const Text("Update Profile "),
-          ),
-          TextButton(
-            onPressed: () {
-              dx.track("Clicked Button", {"gameName": "Tetris"});
-            },
-            child: const Text("Play Game"),
-          ),
-          TextButton(
-            onPressed: () {
-              dx.subscribe();
-            },
-            child: const Text("Subscribe"),
-          ),
-        ],
-      )),
-    ));
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(height: 16),
+            Text(
+              'Login to your account',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            Padding(
+              padding: EdgeInsets.all(24),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(height: 16),
+                  Center(
+                    child: Text(
+                      error_message,
+                      style: TextStyle(color: Colors.red),
+                    ),
+                  ),
+                  SizedBox(height: 16),
+                  Text('Email'),
+                  TextField(
+                    controller: email_controller,
+                    keyboardType: TextInputType.emailAddress,
+                    autofocus: false,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 16),
+                  Text('Password'),
+                  TextField(
+                    controller: password_controller,
+                    obscureText: true,
+                    autofocus: false,
+                    style: TextStyle(color: Colors.black),
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5)),
+                    ),
+                  ),
+                  SizedBox(height: 16),
+                  ElevatedButton(
+                    onPressed: () async {
+                      if (email_controller.text.length == 0 ||
+                          password_controller.text.length == 0) {
+                        error_message = 'Check credentials.';
+                        setState(() {});
+                        print(
+                            'before ${email_controller.text}  ${password_controller.text}');
+                      } else {
+                        setState(() {});
+                        print(
+                            'after ${email_controller.text}  ${password_controller.text}');
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => HomeScreen()));
+                      }
+                    },
+                    child: Container(
+                      alignment: Alignment.center,
+                      child: Text('Login'),
+                    ),
+                  ),
+                  TextButton(
+                    style: ButtonStyle(
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                                    side: BorderSide(color: Colors.blue)))),
+                    onPressed: () {},
+                    child: Container(
+                      alignment: Alignment.center,
+                      child: Text('Register'),
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
